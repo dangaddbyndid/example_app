1a3b3577d995fd5e1a586fb32ba4380469dca47f
Dan Gadd <dan.gadd@beyondidentity.com>
2021-10-06 17:23:12 +0100

    Update InputViewController.swift

--- InputViewController.swift
+++ InputViewController.swift
@@ -9,7 +9,7 @@ class InputViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
         textField.delegate = self
     }
-
+//somecode
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
